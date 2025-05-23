cbc711fd7d9e64af592859eaa40f8385f155d710
Zechariah <zechariahadebayo42@gmail.com>
2025-05-23 15:37:58 +0100

    chore: auth interface update

--- components/Auths/Login.jsx
+++ components/Auths/Login.jsx
@@ -1,41 +1,34 @@
 import React, { useState } from "react";
 import { handleSubmit, handleState } from "../../utils/auths.js";
-import { toggleAuthPageBtnClassList } from "../../utils/helpers.js";
 import Loader from "./Loader.jsx";
 
 export default function Login(){
     const [data, setData] = useState({})
     const [loading, setLoading] = useState(false)
-    const [activeBtns, setActiveBtns] = useState({login: true, signup: false})
-    const {login, signup} = activeBtns;
     return (
-        <div className="login flex flex-col justify-center align-center bg-white text-black w-full h-1/2 rounded-t-3xl">
-            <form method="post" onSubmit={(e)=> handleSubmit(e, setLoading, data)} className="flex flex-col justify-center align-center justify-self-start text-center text-black -mt-32">
-                <div className="auth-shuffle self-center flex flex-row justify-around align-center w-1/2 border-black rounded-3xl bg-slate-200 relative">
-                    <button type="button"
-                        className={login ? "active-btn": "login-btn flex flex-row justify-center align-center rounded-3xl w-1/2 border-yellow-300 text-center pt-2 pb-2 cursor-pointer"}
-                     onClick={(e)=> { toggleAuthPageBtnClassList(e, setActiveBtns)}}>Login
-                    </button>
-                    <button type="button"
-                        className={signup ? "active-btn": "signup-btn flex flex-row justify-center align-center rounded-3xl w-1/2 border-yellow-300 text-center bg-slate-200 pt-2 pb-2 cursor-pointer"}
-                        onClick={(e)=> { toggleAuthPageBtnClassList(e, setActiveBtns)}}>Sign Up
-                    </button>
+        <form method="post" onSubmit={(e)=> handleSubmit(e, setLoading, data)} className="flex flex-col justify-around items-center justify-self-start text-center text-black h-full w-2/3">
+            <div className="email flex flex-row flex-wrap justify-around items-center w-1/2 h-12 shadow rounded-3xl">
+                <label htmlFor="email" className="text-left flex flex-row justify-center items-center mr-3">Email</label>
+                <input type="email" name="email" id="email" onChange={(e)=> handleState(e, setData)} className="text-black text-center w-2/3" placeholder="johndoe@email.com"/>
+            </div>
+            <div className="password flex flex-row flex-wrap  justify-around items-center w-1/2 h-12 shadow rounded-3xl">
+                <label htmlFor="password" className="text-left flex flex-row justify-center items-center mr-3">Password</label>
+                <input type="password" name="password" id="password" onChange={(e)=> handleState(e, setData)} className="text-center text-black w-2/3" placeholder="************"/>
+            </div>
+            <div className="remember-fg-pass flex flex-row justify-around items-center w-1/2">
+                <div className="remember flex flex-row justify-between items-center text-center w-36">
+                    <input type="checkbox" className="justify-self-start flex flex-row justify-center align-center"/>
+                    <h5 className="remember-txt">Remember me</h5>
                 </div>
-                <div className="email flex flex-row justify-center">
-                    <label htmlFor="email" className="text-left flex flex-row justify-center align-center mr-3">Email</label>
-                    <input type="email" name="email" id="email" onChange={(e)=> handleState(e, setData)}/>
-                </div>
-                <div className="password flex flex-row justify-center">
-                    <label htmlFor="password" className="text-left flex flex-row justify-center align-center mr-3">Password</label>
-                    <input type="password" name="password" id="password" onChange={(e)=> handleState(e, setData)}/>
-                </div>
-                <div className="submit flex flex-row justify-center">
-                    {
-                        loading ? <Loader/> : <button type="submit" name="submit" id="submit" className="flex p-16 text-left">Login</button>
-                    }
-                </div>
-            </form>
-        </div>
+                <input type="button" value="Forgot password?" className="fg-pass text-center cursor-pointer"/>
+            </div>
+            <div className="submit flex flex-row justify-center text-center -mt-16">
+                {
+                    loading ? <Loader/> : <button type="submit" name="submit" 
+                    id="submit" className="flex flex-row justify-around align-center border-2 border-black p-3 bg-black w-96 rounded-3xl text-white">Login</button>
+                }
+            </div>
+        </form>
     )
 
 }
--- utils/helpers.js
+++ utils/helpers.js
@@ -14,13 +14,14 @@ export function handleBackToggle(navigator, url=null){
     navigator(url);
 }
 
-export function toggleAuthPageBtnClassList(e, setActiveBtns){
+export function toggleAuthPageBtnClassList(e, setActiveBtns, navigator=null){
     let btn = e.target;
-    btn.classList.add("active-btn");
     if(btn.classList.contains("login-btn")){
       setActiveBtns((prevState) => ({...prevState, login: true, signup: false}))
+      setTimeout(()=> navigator?.("/login"), 500);
     }
     else{
       setActiveBtns((prevState) => ({...prevState, login: false, signup: true}))
+      setTimeout(()=> navigator?.("/signup"), 500);
     }
 }
