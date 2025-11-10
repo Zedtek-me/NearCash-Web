392c284fa63f76a3432aa06995dbb9e803d848d2
Zechariah <zechariahadebayo42@gmail.com>
2025-11-11 00:56:53 +0100

    chore: style update

--- components/Home/TransactionStatusFilter.jsx
+++ components/Home/TransactionStatusFilter.jsx
@@ -15,14 +15,14 @@ const TransactionStatusFilter = ({ statusMap, refetch }) => {
     }
 
     return (
-        <div className="relative max-w-full text-black cursor-pointer bg-white" onClick={handleOpenStatusFilter}>
+        <div className="relative max-w-full text-black cursor-pointer bg-white rounded-xl" onClick={handleOpenStatusFilter}>
             <div className="filter-by flex justify-center items-center">
                 <p className="w-20 font-semibold">Filter by</p>
                 <ChevronDown size={16} className={`rounded-full transform transition-transform duration-300 ${openStatusFilter ? 'rotate-180' : ''}`}/>
             </div>
             {
                 openStatusFilter && (
-                    <div className="absolute left-6 flex flex-col justify-between items-center px-auto mx-auto tex-black z-10 bg-white w-44 h-44 p-5 text-left">
+                    <div className="absolute left-6 flex flex-col justify-between items-center px-auto mx-auto tex-black z-10 bg-white w-44 h-44 p-5 text-left rounded-xl">
                     {
                         statusMap.map((statusData) => (
                         <div className={`text-${statusData?.color}`} onClick={() => handleSearch(statusData?.name)}>
