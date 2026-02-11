6488bc9a1b1a671e70316e6f09a186bda247148f
Zechariah <zechariahadebayo42@gmail.com>
2026-02-12 00:03:27 +0100

    chore: modified webpack config for vercel

---
+++ webpack.config.prod.js
@@ -0,0 +1,62 @@
+const path = require("path")
+const Dotenv = require("dotenv-webpack");
+
+
+module.exports = {
+    mode:  "development",
+    entry: path.resolve(`./src/index.js`),
+    output: {
+        path: path.join(__dirname, "public"),
+        filename: "bundle.js",
+        publicPath: ""
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx|ts)$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"],
+                        plugins: [
+                            "@babel/plugin-transform-runtime"
+                          ],
+                        sourceType: "module"
+                    },
+                },
+            },
+            {
+                test: /\.(css|scss)$/i,
+                use: [
+                    "style-loader", {
+                        loader: "css-loader",
+                        options: {
+                            importLoaders: 1
+                        }
+                    },
+                    "postcss-loader"
+                ],
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/i,
+                type: "asset/resource",
+            }
+        ],
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+          },
+          compress: true,
+          historyApiFallback:true,
+          hot:true,
+          port: 8015,
+    },
+    // plugins: [
+    //     new Dotenv()
+    // ],
+    resolve: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+    }
+}
